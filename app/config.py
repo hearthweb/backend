da9ed28db8af1eb8a92ada345425fb33725bc56f
Nathan Osman <nathan@nathanosman.com>
2026-05-17 02:39:51 -0700

    Add application configuration.

---
+++ app/config.py
@@ -0,0 +1,42 @@
+from enum import Enum
+from pathlib import Path
+from typing import Literal
+
+from pydantic_settings import BaseSettings
+
+
+class Environment(Enum):
+    DEV = "dev"
+    PROD = "prod"
+
+
+class Settings(BaseSettings):
+    # Environment the application is running under
+    ENVIRONMENT: Literal[Environment.DEV, Environment.PROD] = Environment.DEV
+
+    # Location for persistent storage
+    DATA_DIR: str = "data"
+
+    # Database connection information
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = ""
+    DB_HOST: str = "postgres"
+    DB_PORT: int = 5432
+    DB_NAME: str = "postgres"
+
+    @property
+    def DATABASE_URL(self) -> str:
+        if self.ENVIRONMENT == Environment.DEV:
+            return "sqlite:///{path}".format(
+                path=Path(self.DATA_DIR) / "db.sqlite3",
+            )
+        return "postgresql://{user}:{password}@{host}:{port}/{name}".format(
+            user=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            port=self.DB_PORT,
+            name=self.DB_NAME,
+        )
+
+
+settings = Settings()
