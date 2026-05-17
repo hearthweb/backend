0eb030ba9fdefc16f7976200038d7911f1cbc44d
Nathan Osman <nathan@nathanosman.com>
2026-05-17 00:04:24 -0700

    Switch to SQLModel.

--- app/models/user.py
+++ app/models/user.py
@@ -1,30 +1,44 @@
-from sqlalchemy import Boolean, String
-from sqlalchemy.orm import Mapped, mapped_column
-
-from app.db.base import Base
+from sqlalchemy import String
+from sqlmodel import Field, SQLModel
 
 
-class User(Base):
-    """
-    A registered user that may login
-    """
-
-    __tablename__ = "users"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-
-    email: Mapped[str] = mapped_column(
-        String(255),
-        index=True,
+class UserBase(SQLModel):
+    email: str = Field(
+        sa_type=String(255),
         unique=True,
+        index=True,
+    )
+    first_name: str = Field(
+        sa_type=String(100),
+    )
+    last_name: str = Field(
+        sa_type=String(100),
     )
 
-    password: Mapped[str] = mapped_column(String(60))
 
-    first_name: Mapped[str] = mapped_column(String(100))
-    last_name: Mapped[str] = mapped_column(String(100))
+class UserRead(UserBase):
+    id: int
+    is_admin: bool
 
-    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    def __repr__(self) -> str:
-        return f"<User id={self.id} email={self.email}>"
+class UserCreateEdit(UserBase):
+    password: str = Field()
+
+
+class UserCreateEditAdmin(UserCreateEdit):
+    is_admin: bool = Field(
+        default=False,
+    )
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(
+        default=None,
+        primary_key=True,
+    )
+    hashed_password: str = Field(
+        sa_type=String(60),
+    )
+    is_admin: bool = Field(
+        default=False,
+    )
