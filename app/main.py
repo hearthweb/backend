e2895b37ec227a8e04007a33aa36ccf14bafc604
Nathan Osman <nathan@nathanosman.com>
2026-05-23 18:00:22 -0700

    Don't initialize the DB in the main app.

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.config import Environment, settings
-from app.database import init_db
 from app.routers import auth, users
 from app.utils import init_data
 
@@ -13,7 +12,6 @@ async def lifespan(app: FastAPI):
     if settings.ENVIRONMENT == Environment.PROD and settings.SECRET_KEY == "":
         raise RuntimeError("SECRET_KEY must be set in production")
     init_data()
-    init_db()
     yield
 
 
