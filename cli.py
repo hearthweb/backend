6cf61e256d0c6e13dda5ebf1b33782affcc07f7a
Nathan Osman <nathan@nathanosman.com>
2026-05-23 18:00:07 -0700

    Add commands for creating admin users and initializing the database.

---
+++ cli.py
@@ -0,0 +1,49 @@
+from contextlib import contextmanager
+
+import typer
+
+from app.database import get_db, init_db
+from app.models.auth import User
+from app.utils import init_data
+
+app = typer.Typer()
+
+
+@app.callback()
+def startup():
+    init_data()
+
+
+@app.command(
+    help="Create an admin user",
+)
+def create_admin(
+    email: str = typer.Option(prompt=True),
+    password: str = typer.Option(
+        prompt=True,
+        hide_input=True,
+        confirmation_prompt=True,
+    ),
+):
+    with contextmanager(get_db)() as db:
+        user = User(
+            email=email,
+            is_admin=True,
+        )
+        user.set_password(password)
+        db.add(user)
+        db.commit()
+        typer.secho(f"Created admin {email}!", fg=typer.colors.GREEN)
+
+
+@app.command(
+    "init-db",
+    help="Initialize the database",
+)
+def init_db_():
+    init_db()
+    typer.secho("Database initialized!", fg=typer.colors.GREEN)
+
+
+if __name__ == "__main__":
+    app()
