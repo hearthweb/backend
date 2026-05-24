9fc6c57f06e29fc93827eb6f79d54eb0d5919ae0
Nathan Osman <nathan@nathanosman.com>
2026-05-23 21:51:01 -0700

    Remove redundant nullable on Base fields.

--- app/models/base.py
+++ app/models/base.py
@@ -1,19 +1,16 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, func
-from sqlmodel import Field, SQLModel
+from sqlmodel import DateTime, Field, SQLModel, func
 
 
 class Base(SQLModel):
     created_at: datetime = Field(
-        nullable=False,
         sa_type=DateTime(timezone=True),
         sa_column_kwargs={
             "server_default": func.now(),
         },
     )
     updated_at: datetime = Field(
-        nullable=False,
         sa_type=DateTime(timezone=True),
         sa_column_kwargs={
             "onupdate": func.now(),
