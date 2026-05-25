13597720dacf73f2fc3b04d2a867ae077d134f58
Nathan Osman <nathan@nathanosman.com>
2026-05-25 00:17:53 -0700

    Implement two more unit tests.

--- tests/routers/test_users.py
+++ tests/routers/test_users.py
@@ -2,11 +2,37 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.models.user import User, UserRead
+from tests.utils import compare_sorted, dump
 
 
-def test_get_users(client: TestClient, logged_in_user: User):
+def test_users(
+    client: TestClient,
+    logged_in_admin: User,
+    user: User,
+):
     response = client.get("/users")
     assert response.status_code == status.HTTP_200_OK
-    json = response.json()
-    assert len(json) == 1
-    assert json[0] == UserRead.model_validate(logged_in_user).model_dump(mode="json")
+    assert compare_sorted(
+        response.json(),
+        [
+            dump(UserRead, logged_in_admin),
+            dump(UserRead, user),
+        ],
+        "id",
+    )
+
+
+def test_users_me(client: TestClient, logged_in_user: User):
+    response = client.get("/users/me")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == dump(UserRead, logged_in_user)
+
+
+def test_users_user_id(
+    client: TestClient,
+    logged_in_admin: User,
+    user: User,
+):
+    response = client.get(f"/users/{user.id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == dump(UserRead, user)
