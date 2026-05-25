524fd75bbad16d43c0e9dadd449688c37fdd79fb
Nathan Osman <nathan@nathanosman.com>
2026-05-24 23:23:03 -0700

    Add utility function for dumping models.

--- tests/utils.py
+++ tests/utils.py
@@ -1,12 +1,21 @@
 from operator import itemgetter
-from typing import List, TypeVar
+from typing import Any, List, TypeVar
 
-T = TypeVar("T")
+from pydantic import BaseModel
+
+DictT = TypeVar("DictT", bound=dict)
 
 
-def compare_sorted(l1: List[T], l2: List[T], key: str) -> bool:
+def compare_sorted(l1: List[DictT], l2: List[DictT], key: str) -> bool:
     """
     Compare two lists by sorting them using a key
     """
     key_fn = itemgetter(key)
     return sorted(l1, key=key_fn) == sorted(l2, key=key_fn)
+
+
+ModelT = TypeVar("ModelT", bound=BaseModel)
+
+
+def dump(model: type[ModelT], obj: Any) -> dict[str, Any]:
+    return model.model_validate(obj).model_dump(mode="json")
