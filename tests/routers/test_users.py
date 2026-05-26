14f25c68fdc5b4ab8d12866fe260e6b8a3f4ebc9
Nathan Osman <nathan@nathanosman.com>
2026-05-25 22:40:51 -0700

    Test user creation.

--- tests/routers/test_users.py
+++ tests/routers/test_users.py
@@ -1,7 +1,9 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+from sqlmodel import Session
 
 from app.models.user import User, UserRead
+from tests.constants import USER_EMAIL, USER_PASSWORD
 from tests.utils import compare_sorted, dump
 
 
@@ -36,3 +38,20 @@ def test_users_user_id(
     response = client.get(f"/users/{user.id}")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == dump(UserRead, user)
+
+
+def test_users_create(
+    client: TestClient,
+    logged_in_admin: User,
+    db: Session,
+):
+    response = client.post(
+        "/users",
+        json={
+            "email": USER_EMAIL,
+            "password": USER_PASSWORD,
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+    json = response.json()
+    assert json == dump(UserRead, db.get(User, json["id"]))
