dd03c44b5cdb6223651268e1d7a69f1c8e360518
Nathan Osman <nathan@nathanosman.com>
2026-08-22 17:10:54 -0700

    Add router for finance models.

---
+++ app/routers/finance/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from app.routers.finance import accounts, transactions
+
+router = APIRouter(
+    prefix="/finance",
+    tags=["Finance"],
+)
+
+router.include_router(accounts.router)
+router.include_router(transactions.router)
