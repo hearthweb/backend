7e53580b9be85b45756231a1b7232ce3cb94437d
Nathan Osman <nathan@nathanosman.com>
2026-08-23 16:03:11 -0700

    Add tests for Document routes.

---
+++ tests/routers/test_documents.py
@@ -0,0 +1,107 @@
+from pathlib import Path
+
+from fastapi import status
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from app.models.document.category import (
+    Category,
+    CategoryRead,
+)
+from app.models.document.document import (
+    Document,
+    DocumentPublic,
+)
+from app.models.user import User
+from tests.constants import (
+    DOCUMENT_CONTENT,
+    DOCUMENT_FILENAME,
+    DOCUMENT_FILETYPE,
+    DOCUMENT_NAME,
+)
+from tests.utils import compare_sorted, dump
+
+
+def test_documents_categories(
+    client: TestClient,
+    logged_in_admin: User,
+    category: Category,
+):
+    response = client.get("/documents/categories")
+    assert response.status_code == status.HTTP_200_OK
+    assert compare_sorted(
+        response.json(),
+        [
+            dump(CategoryRead, category),
+        ],
+        "id",
+    )
+
+
+def test_documents(
+    client: TestClient,
+    logged_in_admin: User,
+    document: Document,
+):
+    response = client.get("/documents")
+    assert response.status_code == status.HTTP_200_OK
+    assert compare_sorted(
+        response.json(),
+        [
+            dump(DocumentPublic, document),
+        ],
+        "id",
+    )
+
+
+def test_documents_create(
+    client: TestClient,
+    logged_in_admin: User,
+    category: Category,
+    tmp_path: str,
+    db: Session,
+):
+    response = client.post(
+        "/documents",
+        data={
+            "name": DOCUMENT_NAME,
+            "category_id": category.id,
+        },
+        files={
+            "file": (
+                DOCUMENT_FILENAME,
+                DOCUMENT_CONTENT,
+                DOCUMENT_FILETYPE,
+            ),
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK
+    json = response.json()
+    document = db.get(Document, json["id"])
+    assert json == dump(DocumentPublic, document)
+    with open(Path(tmp_path) / document.relative_path, "r") as f:
+        assert f.read() == DOCUMENT_CONTENT
+
+
+def test_documents_by_id_download(
+    client: TestClient,
+    logged_in_admin: User,
+    document: Document,
+    tmp_path: str,
+):
+    response = client.get(f"/documents/{document.id}/download")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert response.headers["X-Accel-Redirect"] == str(
+        Path(tmp_path) / document.relative_path
+    )
+
+
+def test_documents_by_id_delete(
+    client: TestClient,
+    logged_in_admin: User,
+    document: Document,
+    db: Session,
+):
+    response = client.delete(f"/documents/{document.id}")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert db.get(Document, document.id) is None
