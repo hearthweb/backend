0e28973b501ce6753f9290c92bee9db830324b7b
Nathan Osman <nathan@nathanosman.com>
2026-08-29 22:05:53 -0700

    Use permission map in /permissions route.

--- app/auth/routers/permissions.py
+++ app/auth/routers/permissions.py
@@ -1,13 +1,7 @@
-from collections.abc import Generator
+from fastapi import APIRouter
 
-from fastapi import APIRouter, Depends, Request
-from fastapi.routing import APIRoute
-
-from app.auth.dependencies.user import (
-    require_permission,
-    require_permission_responses,
-)
 from app.auth.models.permission import Permission
+from app.permissions import permission_map
 
 router = APIRouter(
     prefix="/permissions",
@@ -18,30 +12,7 @@ router = APIRouter(
 @router.get(
     "",
     summary="Get a list of permissions",
-    dependencies=[Depends(require_permission)],
-    responses={**require_permission_responses},
     operation_id="authPermissions",
 )
-def permissions(
-    request: Request,
-) -> list[Permission]:
-
-    # Each route can also include other routers; traverse recursively
-    def enum_routes(routes) -> Generator[APIRoute]:
-        for r in routes:
-            if isinstance(r, APIRoute):
-                for d in r.dependencies:
-                    if d.dependency == require_permission:
-                        yield r
-            elif hasattr(r, "original_router"):
-                yield from enum_routes(r.original_router.routes)
-            elif hasattr(r, "routes"):
-                yield from enum_routes(r.routes)
-
-    return [
-        Permission(
-            name=r.operation_id,
-            description=r.summary,
-        )
-        for r in enum_routes(request.app.routes)
-    ]
+def permissions() -> list[Permission]:
+    return permission_map.values()
