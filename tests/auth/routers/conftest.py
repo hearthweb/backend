410fea32200b775502019656efe7255d622f20a7
Nathan Osman <nathan@nathanosman.com>
2026-09-06 16:12:15 -0700

    Add tests for remaining TOTP routes.

--- tests/auth/routers/conftest.py
+++ tests/auth/routers/conftest.py
@@ -1,9 +1,10 @@
-import pyotp
 import pytest
 from sqlmodel import Session
 
 from app.auth.models.totp import Totp
 from app.auth.models.user import User
+
+from . import TOTP_SECRET
 
 
 @pytest.fixture(name="totp")
@@ -12,7 +13,19 @@ def totp_fixture(
     user: User,
 ) -> Totp:
     totp = Totp(user_id=user.id)
-    totp.set_secret(pyotp.random_hex())
+    totp.set_secret(TOTP_SECRET)
+    db.add(totp)
+    db.commit()
+    return totp
+
+
+@pytest.fixture(name="totp_verified")
+def totp_verified_fixture(
+    db: Session,
+    totp: Totp,
+) -> Totp:
+    totp.encrypted_secret = totp.encrypted_secret_new
+    totp.encrypted_secret_new = ""
     db.add(totp)
     db.commit()
     return totp
--- tests/auth/routers/test_totp.py
+++ tests/auth/routers/test_totp.py
@@ -1,8 +1,14 @@
+import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
 from app.auth.models.totp import Totp
 from app.auth.models.user import User
+
+from . import (
+    TOTP_CODE,
+    USER_PASSWORD,
+)
 
 
 def test_totp(
@@ -16,3 +22,94 @@ def test_totp(
         "enabled": False,
         "verified": False,
     }
+
+
+@pytest.mark.parametrize(
+    "password, status_code",
+    [
+        (USER_PASSWORD, status.HTTP_200_OK),
+        ("", status.HTTP_401_UNAUTHORIZED),
+    ],
+)
+def test_totp_create(
+    client: TestClient,
+    logged_in_user: User,
+    password: str,
+    status_code: int,
+):
+    response = client.post(
+        "/auth/totp",
+        json={
+            "password": password,
+        },
+    )
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "code, status_code",
+    [
+        (TOTP_CODE, status.HTTP_200_OK),
+        ("", status.HTTP_401_UNAUTHORIZED),
+    ],
+)
+def test_totp_create_existing(
+    client: TestClient,
+    logged_in_user: User,
+    totp_verified: Totp,
+    code: str,
+    status_code: int,
+):
+    response = client.post(
+        "/auth/totp",
+        json={
+            "code": code,
+        },
+    )
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "code, status_code",
+    [
+        (TOTP_CODE, status.HTTP_200_OK),
+        ("", status.HTTP_401_UNAUTHORIZED),
+    ],
+)
+def test_totp_verify(
+    client: TestClient,
+    logged_in_user: User,
+    totp: Totp,
+    code: str,
+    status_code: int,
+):
+    response = client.post(
+        "/auth/totp/verify",
+        json={
+            "code": code,
+        },
+    )
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "code, status_code",
+    [
+        (TOTP_CODE, status.HTTP_204_NO_CONTENT),
+        ("", status.HTTP_401_UNAUTHORIZED),
+    ],
+)
+def test_totp_delete(
+    client: TestClient,
+    logged_in_user: User,
+    totp_verified: Totp,
+    code: str,
+    status_code: int,
+):
+    response = client.delete(
+        "/auth/totp",
+        params={
+            "code": code,
+        },
+    )
+    assert response.status_code == status_code
