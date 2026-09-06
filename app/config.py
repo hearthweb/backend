488a9062aa0cfe4bde7f73ea5d911333cbf2a8c6
Nathan Osman <nathan@nathanosman.com>
2026-09-06 11:51:01 -0700

    Move validation of config to config.py.

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from pydantic import model_validator
 from pydantic_settings import BaseSettings
 
 
@@ -25,7 +26,7 @@ class Settings(BaseSettings):
     # Key used for session management; cannot be empty in prod
     SECRET_KEY: str = ""
 
-    # Key use for encrypting TOTP keys
+    # Key used for encrypting TOTP secrets; cannot be empty in prod
     TOTP_ENCRYPTION_KEY: str = ""
 
     @property
@@ -35,6 +36,15 @@ class Settings(BaseSettings):
                 return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
             case Environment.DEV:
                 return f"sqlite:///{'db.sqlite3'}"
+
+    @model_validator(mode="after")
+    def validate_prod_keys(self) -> Settings:
+        match self.ENVIRONMENT:
+            case Environment.PROD:
+                if settings.SECRET_KEY == "":
+                    raise RuntimeError("SECRET_KEY must be set in production")
+                if settings.TOTP_ENCRYPTION_KEY == "":
+                    raise RuntimeError("TOTP_ENCRYPTION_KEY must be set in production")
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from fastapi import FastAPI
 
 from app.auth.routers import router as auth_router
-from app.config import Environment, settings
+from app.config import settings
 from app.finance.routers import router as finance_router
 from app.internal.routers import router as internal_router
 from app.registry.routers import router as registry_router
@@ -14,8 +14,6 @@ def init():
     """
     Initialize the application
     """
-    if settings.ENVIRONMENT == Environment.PROD and settings.SECRET_KEY == "":
-        raise RuntimeError("SECRET_KEY must be set in production")
     Path(settings.UPLOAD_DIR).mkdir(
         parents=True,
         exist_ok=True,
