bcdf255e50ee52869f8f33c1d06a177273a77d45
Nathan Osman <nathan@nathanosman.com>
2026-09-07 14:46:56 -0700

    Move HTTP exception / response to util.

--- app/auth/routers/sessions.py
+++ app/auth/routers/sessions.py
@@ -26,7 +26,7 @@ from app.auth.models.user import (
     UserRead,
 )
 from app.database import get_db
-from app.types import create_http_exception_response
+from app.util.http import create_http_exception_response
 from app.util.time import get_current_time
 
 router = APIRouter(prefix="/sessions")
